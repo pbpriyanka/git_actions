2ffdf8a1b677a9c8c2bae3449f0374e6dee47d7d
pbpriyanka <priyanka.jain@purposebrands.com>
2025-12-19 13:18:30 +0530

    Update convert.py

--- convert.py
+++ convert.py
@@ -160,7 +160,6 @@ if __name__ == "__main__":
     connection_parameters = {
         "account": os.getenv("SNOWFLAKE_ACCOUNT"),
         "user": os.getenv("SNOWFLAKE_USER"),
-        "password": os.getenv("SNOWFLAKE_PASSWORD"),
         "role": os.getenv("SNOWFLAKE_ROLE"),
         "warehouse": os.getenv("SNOWFLAKE_WAREHOUSE"),
         "database": os.getenv("SNOWFLAKE_DATABASE"),
