8fef0673caa93e810d00b3c3d9f0277e4690bf95
pbpriyanka <priyanka.jain@purposebrands.com>
2025-12-19 20:17:55 +0530

    Update upload_to_stage.py

--- upload_to_stage.py
+++ upload_to_stage.py
@@ -23,7 +23,7 @@ def get_snowflake_session():
         "account": os.environ["SNOWFLAKE_ACCOUNT"],
         "user": os.environ["SNOWFLAKE_USER"],
         "role": os.environ["SNOWFLAKE_ROLE"],
-        "warehouse": os.environ["SNOWFLAKE_WAREHOUSE"]
+        "warehouse": os.environ["SNOWFLAKE_WAREHOUSE"],
         "private_key": private_key_der
     }
 
