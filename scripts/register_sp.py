f10fe2e10973c15526a017b1461ec8d787a90d37
pbpriyanka <priyanka.jain@purposebrands.com>
2025-12-23 18:18:32 +0530

    Delete scripts/register_sp.py

--- scripts/register_sp.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-DIST_DIR = "dist"
-
-for py in Path(DIST_DIR).glob("*.py"):
-    print(f"Registering stored procedure for {py.name}")
-
-print("Dummy stored procedure registration done")
