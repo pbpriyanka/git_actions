6bd0504d425adde4933608b28198bc5bf0354c31
pbpriyanka <priyanka.jain@purposebrands.com>
2025-12-23 19:34:41 +0530

    Update convert.py

--- convert.py
+++ convert.py
@@ -326,7 +326,7 @@ def convert_notebook(notebook_path):
     subprocess.run(
         [
             "jupyter", "nbconvert",
-            "--to", "scripts",
+            "--to", "script",
             cleaned_ipynb,
             "--output", notebook_name,
             "--output-dir", SCRIPTS_DIR
