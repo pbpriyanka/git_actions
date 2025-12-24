f14fa8cb190c1f458f781fcff7a2bfb65369ab0a
pbpriyanka <priyanka.jain@purposebrands.com>
2025-12-24 16:03:56 +0530

    Update create_tasks.py

--- create_tasks.py
+++ create_tasks.py
@@ -6,9 +6,7 @@ conn = connect(
     user=os.environ['SNOWFLAKE_USER'],
     password=os.environ['SNOWFLAKE_PASSWORD'],
     account=os.environ['SNOWFLAKE_ACCOUNT'],
-    warehouse=os.environ['SNOWFLAKE_WAREHOUSE'],
-    database=os.environ['SNOWFLAKE_DATABASE'],
-    schema=os.environ['SNOWFLAKE_SCHEMA']
+    warehouse=os.environ['SNOWFLAKE_WAREHOUSE']
 )
 cur = conn.cursor()
 
