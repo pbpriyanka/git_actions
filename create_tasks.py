3c2429501bb8a2c491856bb73a4a1f6cf7ca4ed0
pbpriyanka <priyanka.jain@purposebrands.com>
2026-01-07 12:23:33 +0530

    Update create_tasks.py

--- create_tasks.py
+++ create_tasks.py
@@ -34,7 +34,7 @@ cur = conn.cursor()
 sp_tasks = [
     ('data_ingestion_task','data_ingestion'),
     ('data_harmonization_task', 'data_harmonization'),
-    ('data_qc_task', 'data_qc'),
+    # ('data_qc_task', 'data_qc'),
     ('missing_value_task', 'missing_value'),
     ('missing_dates_task', 'missing_dates'),
     ('feature_engineering_task', 'feature_engineering'),
